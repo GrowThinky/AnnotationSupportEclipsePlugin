40b8694617e214a701afae9c69a7b02d795f6b5d
Christoph Heindl <christoph@ChristophsMBP2.netgear.com>
2022-09-30 23:59:34 -0400

    set up testing logic using ProjectionDocument

--- de.pltlab.annotationFolding/src/test/Test.java
+++ de.pltlab.annotationFolding/src/test/Test.java
@@ -8,53 +8,109 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaSourceViewer;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.projection.ProjectionDocument;
 import org.eclipse.jface.text.source.AnnotationModel;
 import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotationModel;
+import org.eclipse.jface.text.source.projection.ProjectionViewer;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.junit.jupiter.api.Assertions;
 import org.eclipse.ui.*;
 import org.eclipse.ui.editors.text.TextEditor;
 
 class Test {
 
+	@SuppressWarnings("restriction")
 	@org.junit.jupiter.api.Test
-	void test() throws ExecutionException, CoreException {
+	void test() throws ExecutionException, CoreException, BadLocationException {
 
-//		ExecutionEvent event = null;
-//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-//		IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
-//
-//		MessageDialog.openInformation(window.getShell(), "GenerateBuilderProject",
-//				editorPart.getEditorInput().getName());
-//
-//		IEditorInput input = editorPart.getEditorInput();
-//		if (input instanceof FileEditorInput) {
-//			IFile file = ((FileEditorInput) input).getFile();
-//			InputStream is = file.getContents();
-//			// TODO get contents from InputStream
-//		}
-//		
-		TextEditor editor = (TextEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-			    .getActivePage().getActiveEditor();
-		
-		IDocumentProvider provider = editor.getDocumentProvider();
-		
-			// isInstanceOf
-		ProjectionAnnotationModel model = (ProjectionAnnotationModel) provider.getAnnotationModel(provider);
-		
-		
-		
-		IDocument document = provider.getDocument(model);
-		
-		
-		
-		// IDE.openEditor(	, input, null)
+		JavaEditor editor = (JavaEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.getActiveEditor();
+		IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+		document.set(test1);
+		editor.doSave(null);
+		editor.getDocumentProvider().resetDocument(document);
+
+		if (editor != null) {
+			ProjectionViewer viewer = (ProjectionViewer) editor.getViewer();
+			
+			viewer.collapseAll();
+			viewer.reinitializeProjection();
+
+			ProjectionDocument projectionDocument = viewer.getProjectionTest();
+
+			if (projectionDocument != null) {
+				
+				String foldedText = projectionDocument.get();
+				System.out.println("one" + foldedText);
+
+				Assertions.assertEquals(folded1,foldedText);
+				
+				document.set(test2);
+				editor.doSave(null);
+				
+				viewer.collapseAll();
+				viewer.reinitializeProjection();
+				
+				foldedText = projectionDocument.get();
+				
+				Assertions.assertEquals(folded2,foldedText);			
+				return;
+			}
+
+		}
+		fail();
 
 	}   
-
+	
+	String test1 = "package fruits;\n"
+			+ "\n"
+			+ "public class fruitTest {\n"
+			+ "	\n"
+			+ "	\n"
+			+ "	@SuppressWarnings({ \"restriction\",\"unused\",\"restriction\", \"unused\"  })\n"
+			+ "	@Finished(value = \"Class scope\", priority = Unfinished.Priority.LOW)\n"
+			+ "	public void juice() {\n"
+			+ "		System.out.println(\"Fruit Juice!\");\n"
+			+ "	}\n"
+			+ "}";
+	
+	String folded1 = "package fruits;\n"
+			+ "\n"
+			+ "public class fruitTest {\n"
+			+ "	\n"
+			+ "	\n"
+			+ "	\n"
+			+ "	public void juice() {\n"
+			+ "}";
+	
+	String test2 = "package fruits;\n"
+			+ "\n"
+			+ "public class fruitTest {\n"
+			+ "	\n"
+			+ "	\n"
+			+ "	@SuppressWarnings(\"unused\")\n"
+			+ "	public void peel() {\n"
+			+ "		System.out.println(\"Fruit Peel!\");\n"
+			+ "	}\n"
+			+ "}";
+	
+	String folded2 = "package fruits;\n"
+			+ "\n"
+			+ "public class fruitTest {\n"
+			+ "	\n"
+			+ "	\n"
+			+ "	public void peel() {\n"
+			+ "}";
+			
 }
